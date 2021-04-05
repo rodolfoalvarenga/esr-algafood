3ae6cd4e79fd09739552a4b63885e599b627e86c
Rodolfo Luiz Alvarenga <CIANDT\rodolfoalvarenga@lnb028672cps.ciandt.global>
2021-04-04 21:21:39 -0300

    3_20 Desafio: mapeando entidades

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -14,31 +14,31 @@ import com.algaworks.algafood.domain.repository.RestauranteRepository;
 @Component
 public class RestauranteRepositoryImpl implements RestauranteRepository {
 
-    @PersistenceContext
-    private EntityManager manager;
-    
-    @Override
-    public List<Restaurante> listar() {
-        return manager.createQuery("from Restaurante", Restaurante.class)
-                .getResultList();
-    }
-    
-    @Override
-    public Restaurante buscar(Long id) {
-        return manager.find(Restaurante.class, id);
-    }
-    
-    @Transactional
-    @Override
-    public Restaurante salvar(Restaurante restaurante) {
-        return manager.merge(restaurante);
-    }
-    
-    @Transactional
-    @Override
-    public void remover(Restaurante restaurante) {
-        restaurante = buscar(restaurante.getId());
-        manager.remove(restaurante);
-    }
+	@PersistenceContext
+	private EntityManager manager;
+	
+	@Override
+	public List<Restaurante> listar() {
+		return manager.createQuery("from Restaurante", Restaurante.class)
+				.getResultList();
+	}
+	
+	@Override
+	public Restaurante buscar(Long id) {
+		return manager.find(Restaurante.class, id);
+	}
+	
+	@Transactional
+	@Override
+	public Restaurante salvar(Restaurante restaurante) {
+		return manager.merge(restaurante);
+	}
+	
+	@Transactional
+	@Override
+	public void remover(Restaurante restaurante) {
+		restaurante = buscar(restaurante.getId());
+		manager.remove(restaurante);
+	}
 
 }
--- src/main/java/com/algaworks/algafood/jpa/ConsultaCidadeMain.java
+++ src/main/java/com/algaworks/algafood/jpa/ConsultaCidadeMain.java
@@ -1,25 +1,29 @@
 package com.algaworks.algafood.jpa;
 
+import java.util.List;
+
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
 import com.algaworks.algafood.AlgafoodApiApplication;
-import com.algaworks.algafood.domain.model.Restaurante;
-import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.repository.CidadeRepository;
 
-public class BuscarRestaurante {
+public class ConsultaCidadeMain {
 
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
 				.web(WebApplicationType.NONE)
 				.run(args);
 		
-		RestauranteRepository restauranteRepository = applicationContext.getBean(RestauranteRepository.class);
+		CidadeRepository cidadeRepository = applicationContext.getBean(CidadeRepository.class);
 		
-		Restaurante restaurante = restauranteRepository.buscar(1L);
+		List<Cidade> todasCidades = cidadeRepository.listar();
 		
-		System.out.println(restaurante.getNome() + " " + restaurante.getTaxaFrete());
+		for (Cidade cidade : todasCidades) {
+			System.out.printf("%s - %s\n", cidade.getNome(), cidade.getEstado().getNome());
+		}
 	}
 	
 }
